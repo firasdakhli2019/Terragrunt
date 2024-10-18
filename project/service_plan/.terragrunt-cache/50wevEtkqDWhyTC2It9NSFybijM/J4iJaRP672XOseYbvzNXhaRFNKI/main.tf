351b9a1f38e711401ff5ede90876dbe911f343fa
firasdakhli2019 <firasdakhli2019@gmail.com>
2024-10-18 16:54:28 +0200

    untrack files contained in the .gitignore file

--- project/service_plan/.terragrunt-cache/50wevEtkqDWhyTC2It9NSFybijM/J4iJaRP672XOseYbvzNXhaRFNKI/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_service_plan" "tfupskilling-service-plan" {
-  location            = var.location
-  name                = var.service_plan_name
-  os_type             = "Linux"
-  resource_group_name = var.resource_group
-  sku_name            = "B1"
-}
--- project/service_plan/.terragrunt-cache/50wevEtkqDWhyTC2It9NSFybijM/J4iJaRP672XOseYbvzNXhaRFNKI/output.tf
+++
@@ -1,3 +0,0 @@
-output "service_plan_id" {
-  value = azurerm_service_plan.tfupskilling-service-plan.id
-}
